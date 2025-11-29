2847aea79949f106bba3519e16fd9c4d1dc204b2
Mitchee Revel <editionsrevel@gmail.com>
2025-11-29 05:58:53 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,10 +4,10 @@
 set -e
 
 # Install dependencies using pnpm
-pnpm install
+npm install
 
 # Build the application
-pnpm run build
+npm run build
 
 # Start the Node.js server produced by SvelteKit's adapter-node
 # The server will respect the PORT environment variable set by the deployment environment.
